c06ae106a939867cc93b811ff1f8092985c4ed06
Bhuvan Yadav <52439642+bruvan@users.noreply.github.com>
2022-03-03 20:15:46 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,28 +4,28 @@ form = pd.read_excel("spreadsheet.xlsx") #location of the excel file
 list = form['NAME'].to_list() # Name Column
 list1 = form['EVENT'].to_list() # Event Column
 for i in list:
-    im = Image.open('Certificate.png') # certificate image
+    im = Image.open('Certificate.png') # blank certificate image
     imgh= im.height
     w=(im.width)
     d = ImageDraw.Draw(im)
-    font = ImageFont.truetype("SF-Pro-Display-Medium.otf", 52) # font file and size
+    font = ImageFont.truetype("SF-Pro-Display-Medium.otf", 52) # font file and size for writing
 
     t= d.textsize(i,font=font)[0]
     location = (
-        ((w - t)/2)-292, 670) # location of name
+        ((w - t)/2)-292, 670) # location of name placeholder
     text_color = (0, 0, 0)
     d.text(location,i , fill=text_color,font=font)
-    fill_color = "White"  # your background
+    fill_color = "White"  # background color
     for n in list1:
         t = d.textsize(n, font=font)[0]
         location = (
-            ((w - t) / 2) - 292, 834) # location of event 
+            ((w - t) / 2) - 292, 834) # location of event placeholder
         text_color = (0, 0, 0)
         d.text(location, n, fill=text_color, font=font)
-        fill_color = "White"  # your background
+        fill_color = "White"  # background color 
     if im.mode in ('RGBA'):
         background = Image.new(im.mode[:-1], im.size, fill_color)
         background.paste(im, im.split()[-1])
         im = background
 
-    im.save("certificate_"+i+".pdf")
+    im.save("certificate_"+i+".pdf") # certificate saved as certificate_name.pdf
